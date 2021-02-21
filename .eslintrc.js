e1053c214af5586898d9239605122c742da6f81a
Stanley Barnabas Nagwere <stanleybarna@hotmail.com>
2021-02-21 22:02:50 +0300

    modify lint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = exports = {
-    'parser': "babel-eslint",
-    'rules' : {
-    
-        'no-console' : process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "parserOptions": {
+        "parser": "babel-eslint",
+        "sourceType": "module"
+    },
+    'rules': {
+
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'vue/no-unused-components': 'off'
-        }
+    }
 
 }
